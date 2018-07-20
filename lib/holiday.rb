d31fd928a484c2f063b690df082914407901e9dd
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-20 21:16:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,8 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-binding.pry
-
+  
   holiday_hash.each do |season, value|
     puts "'#{season}''.to_s.capitalize:"
   end
