263ac87da61d7ed318139e57107a2fd8d75e0647
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-20 21:19:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts '"season.to_s.capitalize":'
   end
   holiday_hash.each do |holiday, supply_array|
-    puts "#{holiday}.to_s.split("_").map {|w| w.capitalize.join(' ')}: #{supply_array.join(", ")}"
+    puts "'holiday.to_s.split("_").map {|w| w.capitalize.join(' ')}": "supply_array.join(", ")"
   end
 
 end
